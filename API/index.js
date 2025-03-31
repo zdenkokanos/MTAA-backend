c053086d22104004c8506a44871a9da513ff92f6
Zdenko Kanoš <xkanos@stuba.sk>
2025-03-31 09:42:09 +0200

    Added to new files, 3 new queries done (#3)
    * testing
    
    * added insertion and login
    
    * added few files and tournament query

--- API/index.js
+++ API/index.js
@@ -1,11 +1,12 @@
-//// index.js
 // Importing required libraries
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 // Import other modules
-const db = require('./queries')
+const dbUser = require('./userQueries')
+const dbTournament = require('./tournamentQueries')
+const dbTicket = require('./ticketQueries')
 
 // Middleware that parses incoming requests with JSON payloads
 app.use(bodyParser.json())
@@ -19,11 +20,16 @@ app.use(
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 })
-
+//Get
+app.get('/tournaments', dbTournament.getTournaments);
 // Define routes, endpoints for functionality
-app.get('/users', db.getUsers);
-app.get('/users/info/:id', db.getUserInfo);
-app.get('/users/id/:email', db.getUserId);
+app.get('/users', dbUser.getUsers);
+app.get('/users/info/:id', dbUser.getUserInfo);
+app.get('/users/id/:email', dbUser.getUserId);
+
+//Post
+app.post('/users', dbUser.insertUser);
+app.post('/users/login', dbUser.loginUser);
 
 // Start the server and listen on specified port
 app.listen(port, () => {
